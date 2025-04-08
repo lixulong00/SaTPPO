4169ad32da35ea0d5b7dfb1d4e5d9c757afa46f9
XL LI <73598137+lixulong00@users.noreply.github.com>
2025-04-08 18:31:27 +0800

    Update NS2CO_env.py

--- NS2CO_env.py
+++ NS2CO_env.py
@@ -10,19 +10,15 @@ term2 = P_ind * ((np.sqrt(1 + ((V_m ** 4) / (4 * 4.03 * 4.03))) - ((V_m * V_m) /
 term3 = (1. / 2.) * 0.6 * 1.225 * 0.05 * 0.503 * (V_m ** 3)
 T_n_hover = 0.01
 
-
-# The distribution (mean, variance) of data size, complexity, and latency constraints of real computational tasks.
-Task_size_mean, Task_size_std = [40000, 10000, 20000], [40,  10,  20]
-Task_size_max, Task_size_min = Task_size_mean[0] + Task_size_std[0]*3, Task_size_mean[1] - Task_size_std[1]*3
-Task_cpu_mean, Task_cpu_std   = [187.5, 3000, 750], [0.1875, 3, 0.75]
-Task_cpu_max, Task_cpu_min = Task_cpu_mean[1] + Task_cpu_std[1]*3, Task_cpu_mean[0] - Task_cpu_std[0]*3
-Task_delay_mean, Task_delay_std = [0.055, 0.055, 0.04125], [0.00005, 0.00005, 0.0000375]
-Task_delay_max, Task_delay_min = Task_delay_mean[0] + Task_delay_std[0]*3, Task_delay_mean[2] - Task_delay_std[2]*3
-
-
+# Load simulation data sets that are randomly generated for code runnability. Please replace the simulation data set with the real data set, or further adjust the relevant parameters for better optimization results.
+Task_all = np.load('SimulationDdata1.npy', allow_pickle=True)
+Task_size_max, Task_size_min = Task_all[:, :, :, 0].max(), Task_all[:, :, :, 0].min()
+Task_cpu_max, Task_cpu_min = Task_all[:, :, :, 1].max(), Task_all[:, :, :, 1].min()
+Task_delay_max, Task_delay_min = Task_all[:, :, :, 2].max(), Task_all[:, :, :, 2].min()
 
 class env(gym.Env):
     def __init__(self, args):
+        args.IoT_tra_power_max = 1.0
         self.args = args
         self.args.W = self.args.W * 1e6
         self.args.CPU_F_U = self.args.CPU_F_U * 1e9
@@ -30,51 +26,39 @@ class env(gym.Env):
         self.UAV_n = self.args.N
         self.IoT_n = self.args.M
         self.Task_type_n = self.args.Task_type_n
-        self.IoT_xy_init = np.load('IoT_xy_init.npy')
         self.UAV_xyz_init = np.array([[0.,0.,50.]])
         self.IoT_tra_power = np.ones((self.IoT_n,)) * args.IoT_tra_power_max
         self.Task = np.zeros((self.IoT_n, 3))
-        self.rate_task_type_idex = 0
+        
+        self.action_S_dim =  2 * (self.Task_type_n + 1) * self.UAV_n
+        self.action_U_dim = 3*self.UAV_n + 2*self.IoT_n
+        self.state_dim =  1 + self.Task_type_n + self.Task_type_n * 2 + self.UAV_n * 4 + 8 * self.IoT_n
+        self.action_dim = self.action_S_dim + self.action_U_dim
+        
         self.S_action_n =  2 * (self.Task_type_n + 1) * self.UAV_n
         self.U_action_n = 3*self.UAV_n + 2*self.IoT_n
         self.state_n = 1 + self.Task_type_n + self.Task_type_n * 2 + self.UAV_n * 4 + 8 * self.IoT_n
         self.action_n = self.S_action_n + self.U_action_n
+        
         self.cost0 = -10
 
     def reset(self):
+        self.Task_all_n = Task_all[np.random.randint(0, Task_all.shape[0])]
         self.IoT_Task_type = np.random.randint(0, self.Task_type_n, (self.IoT_n,),dtype=np.int32)
         self.T_n = 0
-        self.gen_Task()
         self.UAV_E = np.ones((self.UAV_n,)) * self.args.E_UAV_max
         self.slice_comp = np.zeros((self.UAV_n, (self.Task_type_n + 1)))
         self.slice_comm = np.zeros((self.UAV_n, (self.Task_type_n + 1)))
         self.slice_act = -1
         self.UAV_IoT = np.zeros((self.IoT_n,),dtype=np.int32)
         self.UAV_xyz = self.UAV_xyz_init[:self.UAV_n, :]
-        self.IoT_xy = self.IoT_xy_init[:self.IoT_n, :]
         return self.get_state()
 
     def gen_Task(self):
         # Generate computational tasks based on the distribution (mean, variance, etc.) of data size, complexity, latency constraints, and task types of real computational tasks.
-        time = np.array([0, 30, 20, 15, 10, 5, 6, 8, 6, 5, 7, 10, 20, 30, 28])
-        for i in range(1, len(time)):
-            time[i] = time[i] + time[i - 1]
-        rate_task_type = np.array([[7,  6, 7],[5, 12, 3],[13, 3, 4],[2,  4, 14],[10, 5, 5], [4, 11, 5],[6, 4, 10]])
-        if self.T_n in time:
-            self.rate_task_type_idex = (self.rate_task_type_idex + np.random.randint(1, len(rate_task_type),dtype=np.int32)) % len(rate_task_type)
-            tp_rate_task_type = rate_task_type[self.rate_task_type_idex, :].copy()
-            self.IoT_Task_type = np.random.choice(np.array([0,1,2]), self.IoT_n, replace=True, p=tp_rate_task_type / self.IoT_n)
+        self.Task = self.Task_all_n[self.T_n, :, :3]
+        self.IoT_Task_type = self.Task_all_n[self.T_n, :, 3].astype(np.int32)
 
-        for m in range(self.IoT_n):
-            self.Task[m,0] = np.clip(np.abs(np.random.normal(Task_size_mean[self.IoT_Task_type[m]], Task_size_std[self.IoT_Task_type[m]])), 
-                                     Task_size_mean[self.IoT_Task_type[m]] - 3 * Task_size_std[self.IoT_Task_type[m]], 
-                                     Task_size_mean[self.IoT_Task_type[m]] + 3 * Task_size_std[self.IoT_Task_type[m]])
-            self.Task[m,1] = np.clip(np.abs(np.random.normal(Task_cpu_mean[self.IoT_Task_type[m]], Task_cpu_std[self.IoT_Task_type[m]])), 
-                                     Task_cpu_mean[self.IoT_Task_type[m]] - 3 * Task_cpu_std[self.IoT_Task_type[m]], 
-                                     Task_cpu_mean[self.IoT_Task_type[m]] + 3 * Task_cpu_std[self.IoT_Task_type[m]])
-            self.Task[m,2] = np.clip(np.abs(np.random.normal(Task_delay_mean[self.IoT_Task_type[m]], Task_delay_std[self.IoT_Task_type[m]])), 
-                                     Task_delay_mean[self.IoT_Task_type[m]] - 3 * Task_delay_std[self.IoT_Task_type[m]], 
-                                     Task_delay_mean[self.IoT_Task_type[m]] + 3 * Task_delay_std[self.IoT_Task_type[m]])
     def tmp_sl_action(self, slice_act):
         slice_act = (slice_act + 1 + 1e-20) / 2
         new_slice_comp = slice_act[0 : (self.Task_type_n + 1)*self.UAV_n].reshape((self.UAV_n, (self.Task_type_n + 1)))
@@ -84,8 +68,8 @@ class env(gym.Env):
         new_slice_comm = new_slice_comm / new_slice_comm.sum()
         return new_slice_comp, new_slice_comm
 
-    def tmp_new_state(self, slice_act):
-        slice_act = (slice_act + 1 + 1e-20) / 2
+    def new_state_cost(self, slice_act):
+        slice_act = (slice_act[0] + 1 + 1e-20) / 2
         new_slice_comp = slice_act[0 : (self.Task_type_n + 1)*self.UAV_n].reshape((self.UAV_n, (self.Task_type_n + 1)))
         new_slice_comp = new_slice_comp / new_slice_comp.sum(axis=1, keepdims=True)
 
@@ -113,7 +97,7 @@ class env(gym.Env):
         slice_comp_reCost = (slice_comp_reCost > 0) * slice_comp_reCost
         slice_comm_reCost = new_slice_comm[:, :-1]- self.slice_comm[:, :-1]
         slice_comm_reCost = (slice_comm_reCost > 0) * slice_comm_reCost
-        cost = (self.cost0 - (slice_comm_reCost.sum()+ slice_comp_reCost.sum()) * 10) / (self.cost0 * self.cost0)
+        cost = (self.cost0 - (slice_comm_reCost.sum()+ slice_comp_reCost.sum()) * 10)
         return np.array(state), cost
     def cost_fun(self,slice_comm_reCost, slice_comp_reCost):
         return self.cost0 - (slice_comm_reCost.sum()+ slice_comp_reCost.sum()) * 10
@@ -142,6 +126,8 @@ class env(gym.Env):
                         self.IoT_comp[m] = tmp_comp[m] * self.slice_comp[n][i]
                         self.IoT_comm[m] = tmp_comm[m] * self.slice_comm[n][i]
     def get_state(self):
+        self.gen_Task()
+        self.IoT_xy = self.Task_all_n[self.T_n, :, 4:]
         state = []
         state.append(self.T_n / self.args.T_max)
         for n in range(self.UAV_n):
@@ -244,8 +230,6 @@ class env(gym.Env):
                 self.UAV_E[n] = 0
 
         
-        for m in range(self.IoT_n):
-            self.IoT_xy[m, :] = self.IoT_xy[m, :] + np.random.uniform(-3, 3, self.IoT_xy[m, :].shape)
 
         rr1 = 1 - self.slice_comp[:,-1].mean()
         rr2 = 1 - self.slice_comm[:,-1].sum()
@@ -253,13 +237,12 @@ class env(gym.Env):
         reward = reward + self.cost
 
         self.T_n += 1
-        if self.T_n > self.args.T_max:
+        if self.T_n >= self.args.T_max:
             done = True
         else:
             done = False
         
         info = {}
-        self.gen_Task()
         # reward shaping
-        reward = reward / 20
+        reward = reward
         return self.get_state(), reward, done, info
